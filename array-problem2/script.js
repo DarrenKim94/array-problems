8f6bd9f858e85a52324d9fe801217a92b44895ba
Darren Kim <dkim555@yahoo.com>
2023-01-11 11:57:36 -0800

    write to join function

--- array-problem2/script.js
+++ array-problem2/script.js
@@ -1,3 +1,11 @@
+// Define a function that takes in an array of elements and joins
+// them together into a string separated by a comma with a space after the comma.
+
+// define a function and a parameter
 function joinArray(arr) {
-    
+    // use join instead of toString, join lets you iuse any seperater 
+    console.log(arr.join(' ,'));
 }
+
+const fruits = ['kiwi', 'apple', 'orange','melon']
+joinArray(fruits)
