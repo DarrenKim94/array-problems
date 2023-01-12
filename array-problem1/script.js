6bce79a51407beb8e346d2b1e4fd86da93d40542
Darren Kim <dkim555@yahoo.com>
2023-01-11 21:40:45 -0800

    add if statements

--- array-problem1/script.js
+++ array-problem1/script.js
@@ -3,8 +3,19 @@
 
 // Write a function with two parameters
 function getElements(array, n) {
-    // slice the array starting from the index 0 to n, 
-    // n being the number that is passed in the argument
+    // check to see if a valid array and number is entered
+    if (array === null || n === null){
+      return 'Not a valid input, please try again'
+    }
+     // check if the number is greater than 0
+     else if (n < 0) {
+      return "Please enter a number greater than 0"
+    }
+    // check to see if the number is lesser than the amount of items in the array
+    else if (n > array.length) {
+      return `There are only ${arr.length} item, please enter a new number`
+    }
+    // if everything is correct slice the array starting from the index of 0
     return array.slice(0, n)
   }
     const fruits = ['apple', 'orange', 'bannana', 'kiwi']
