b17ce2a608feb5572a915c34a4bbd1867d5950c4
Darren Kim <dkim555@yahoo.com>
2023-01-11 20:53:22 -0800

    write for loop

--- array-problem4/script.js
+++ array-problem4/script.js
@@ -1,6 +1,19 @@
 // Write a JavaScript function to remove duplicate items from an array 
 // (consider case sensitivity).
 
-function removDup(arr) {
-    
-}
+function removeDuplicates(array) {
+    // make a variable with an empty array to push the items into it
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        // if the items from the array doesnt exist in the newArray
+      if (!newArray.includes(array[i])) {
+        // push it into the new array
+        newArray.push(array[i])
+      }
+    }
+    return newArray
+  }
+  
+  const num = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+  removeDuplicates(num)
+
