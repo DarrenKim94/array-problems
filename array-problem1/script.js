256907faf84885b8ad914e11938754ef64b701ff
Darren Kim <dkim555@yahoo.com>
2023-01-14 10:26:37 -0800

    fix typo in arr

--- array-problem1/script.js
+++ array-problem1/script.js
@@ -2,21 +2,21 @@
 // and return the first n elements of that array.
 
 // Write a function with two parameters
-function getElements(array, n) {
+function getElements(array, num) {
     // check to see if a valid array and number is entered
-    if (array === null || n === null){
+    if (array === null || num === null){
       return 'Not a valid input, please try again'
     }
      // check if the number is greater than 0
-     else if (n < 0) {
+     else if (num < 0) {
       return "Please enter a number greater than 0"
     }
     // check to see if the number is lesser than the amount of items in the array
-    else if (n > array.length) {
-      return `There are only ${arr.length} item, please enter a new number`
+    else if (num > array.length) {
+      return `There are only ${array.length} item, please enter a new number`
     }
     // if everything is correct slice the array starting from the index of 0
-    return array.slice(0, n)
+    return array.slice(0, num)
   }
     const fruits = ['apple', 'orange', 'bannana', 'kiwi']
     getElements(fruits, 2)
